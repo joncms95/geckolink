5ed2429c5c6c63bc5f6856bc6297d80efe459f2e
Jonathan Chan <joncms95@hotmail.com>
2026-05-17 17:05:09 +0800

    Revert "feat(keep-alive): add polling job to keep backend render server up"
    This reverts commit 04decab7bd7a7a3706b8786bb31875d408619e83.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ import Header from "./components/Header";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
-import useKeepAlive from "./hooks/useKeepAlive";
 
 const ParticleBackground = lazy(
   () => import("./components/ParticleBackground"),
@@ -81,8 +80,6 @@ function AppContent() {
 }
 
 export default function App() {
-  // keep Render backend awake when frontend is deployed (set VITE_KEEP_ALIVE_URL)
-  useKeepAlive();
   return (
     <BrowserRouter>
       <AuthProvider>
